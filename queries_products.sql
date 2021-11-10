851ee1925355652d62f4c66cd8e386879750bc01
jade0304 <stefanie.dn88@gmail.com>
2021-11-10 11:23:47 -0600

    sql

---
+++ queries_products.sql
@@ -0,0 +1,15 @@
+-- Comments in SQL Start with dash-dash --
+INSERT INTO products (name, price, can_be_returned) VALUES ('chair', '44.00', 'false');
+INSERT INTO products (name, price, can_be_returned) VALUES ('stool', '25.99', 'true');
+INSERT INTO products (name, price, can_be_returned) VALUES ('table', '124.00', 'false');
+SELECT * FROM products;
+SELECT name FROM products;
+SELECT name, price FROM products;
+INSERT INTO products (name, price, can_be_returned) VALUES ('sectional', '800', 'false');
+SELECT name FROM products WHERE can_be_returned = true GROUP BY name;
+SELECT name FROM products WHERE price < 44 GROUP BY name;
+SELECT name FROM products WHERE price BETWEEN 22.50 AND 99.99;
+UPDATE products SET price = price - 20;
+DELETE FROM products WHERE price < 25;
+UPDATE products SET price = price + 20;
+UPDATE products SET can_be_returned = true;
